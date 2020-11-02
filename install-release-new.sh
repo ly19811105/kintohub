7fca34d31e9a7ff69eba8e749151621950c5d2c5
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-11-02 16:51:09 +0800

    Update install-release-new.sh

--- install-release-new.sh
+++ install-release-new.sh
@@ -8,10 +8,17 @@ v6="r"
 v7="a"
 add1="${v1}${v2}${v3}${v4}${v5}"
 add2="${v1}${v2}${v6}${v7}${v5}"
-mkdir -p /tmp/${add2}
-mkdir -p /tmp/${add1}
-VER=`curl -s https://github.com/'$add1'/'$add2'-core/releases|grep /'$add2'--core/releases/tag/|head -1|awk -F "[/]" '{print $6}'|awk -F "[>]" '{print $2}'|awk -F "[<]" '{print $1}'`
-mkdir -p /tmp/${VER}
+add3="https://api.github.com/repos/${add1}/${add2}-core/releases/latest"
+echo $add3
+TMP_FILE="$(mktemp)"
+        if ! curl -o "$TMP_FILE" ${add3}; then
+            rm "$TMP_FILE"
+            echo 'error: Failed to get release list, please check your network.'
+            exit 1
+        fi
+        EVR="$(sed 'y/,/\n/' "$TMP_FILE" | grep 'tag_name' | awk -F '"' '{print $4}')"
+        rm "$TMP_FILE"
+echo $EVR
 # VER="v4.28.4"
 DOWNLOAD_LINK="https://github.com/${add1}/${add2}-core/releases/download/${VER}/${add2}-linux-64.zip"
 CONFIG_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/config2.json"
