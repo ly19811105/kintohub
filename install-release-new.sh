a782af133590df00c938d37e2dbf7ac5e5f31cdc
ly19811105 <59520732+ly19811105@users.noreply.github.com>
2020-11-02 14:56:15 +0800

    Update install-release-new.sh

--- install-release-new.sh
+++ install-release-new.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-VER=`curl -s https://github.com/v2fly/v2ray-core/releases|grep /v2ray-core/releases/tag/|head -1|awk -F "[/]" '{print $6}'|awk -F "[>]" '{print $2}'|awk -F "[<]" '{print $1}'`
+v1="v"
+v2="2"
+v3="f"
+v4="l"
+v5="y"
+v6="r"
+v7="a"
+add1=${v1}${v2}${v3}${v4}${v5}
+add2=${v1}${v2}${v6}${v7}${v5}
+VER=`curl -s https://github.com/${add1}/${add2}-core/releases|grep /${add2}--core/releases/tag/|head -1|awk -F "[/]" '{print $6}'|awk -F "[>]" '{print $2}'|awk -F "[<]" '{print $1}'`
 # VER="v4.28.4"
-# rm -rf /tmp/Mywebtest/v2ray-linux-64.zip
-
-DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/${VER}/v2ray-linux-64.zip"
-CONFIG_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/config.json"
+DOWNLOAD_LINK="https://github.com/${add1}/${add2}-core/releases/download/${VER}/${add2}-linux-64.zip"
+CONFIG_LINK="https://raw.githubusercontent.com/ly19811105/ray-kintohub/master/config2.json"
 
 mkdir -p /tmp/nginx
 # Install nginx binary to /usr/bin/nginx
@@ -14,12 +21,12 @@ curl -L -o "/tmp/nginx/nginx.zip" ${DOWNLOAD_LINK}
 unzip "/tmp/nginx/nginx.zip" -d "/usr/bin/nginx/"
 rm -rf /tmp/nginx/nginx.zip
 curl -L -o "/etc/nginx/config.json" ${CONFIG_LINK}
+# test add BBR 
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 # Create folder for nginx log
 mkdir -p /var/log/nginx
-ls /usr/bin/nginx/
-mv /usr/bin/nginx/v2ray /usr/bin/nginx/nginx
-rm -rf /usr/bin/nginx/v2ray
+mv /usr/bin/nginx/${add2} /usr/bin/nginx/nginx
+rm -rf /usr/bin/nginx/${add2}
 chmod +x "/usr/bin/nginx/nginx"
 /usr/bin/nginx/nginx -config /etc/nginx/config.json
